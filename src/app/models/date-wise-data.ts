fb0fb4f7ee154c89ae1569e4d214bc5bdbe6e9c4
imdeepakmishra <deepakmishra3813@gmail.com>
2021-04-18 15:43:50 +0530

    added google-charts in countries

---
+++ src/app/models/date-wise-data.ts
@@ -0,0 +1,5 @@
+export interface DateWisedata {
+  country?: string;
+  cases?: number;
+  date?: Date;
+}
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { count, map } from 'rxjs/operators';
+import { DateWisedata } from '../models/date-wise-data';
 import { GlobalDataSummary } from '../models/global-data';
 
 @Injectable({
@@ -12,10 +13,12 @@ export class DataServiceService {
   private globalDataUrl =
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-16-2021.csv';
 
+  private dateWiseDataUrl =
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+
   getGlobalData() {
     return this.http.get(this.globalDataUrl, { responseType: 'text' }).pipe(
       map((result) => {
-        let data: GlobalDataSummary[] = [];
         let raw = {};
         let rows = result.split('\n');
         rows.splice(0, 1);
@@ -44,6 +47,35 @@ export class DataServiceService {
         });
 
         return <GlobalDataSummary[]>Object.values(raw);
+      })
+    );
+  }
+
+  getDateWiseData() {
+    return this.http.get(this.dateWiseDataUrl, { responseType: 'text' }).pipe(
+      map((result) => {
+        let rows = result.split('\n');
+        let header = rows[0];
+        let dates = header.split(/,(?=\S)/);
+        let mainData = [];
+        dates.splice(0, 4);
+        rows.splice(0, 1);
+        rows.forEach((row) => {
+          let cols = row.split(/,(?=\S)/);
+          let con = cols[1];
+          cols.splice(0, 4);
+          mainData[con] = [];
+          cols.forEach((value, index) => {
+            let dw: DateWisedata = {
+              cases: +value,
+              country: con,
+              date: new Date(Date.parse(dates[index])),
+            };
+            mainData[con].push(dw);
+          });
+        });
+
+        return mainData;
       })
     );
   }
